dedf2db1c8b9000ad8236fd78b8390b90cba0a7d
Dionisi Lola <dionisislolas@gmail.com>
2021-07-14 14:03:35 +0100

    Initial commit.

---
+++ src/main/java/com/example/cdladv/CdlAdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdladv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdlAdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdlAdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cdladv/CdlAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdladv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdlAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
